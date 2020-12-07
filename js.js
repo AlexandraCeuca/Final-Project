c7574dcc496834292bf19b031dd202bdbea4b62c
Ceuca <adda.ceuca@yahoo.com>
2020-12-07 20:27:59 +0200

    new structure

---
+++ js.js
@@ -0,0 +1,6 @@
+function load_home() {
+    document.getElementById("header").innerHTML='<object type="text/html" data="header.html" ></object>';
+}
+function load_homeCss() {
+    document.getElementById("header").innerHTML='<object type="stylesheet/css" data="header.css" ></object>';
+}
