e08cc214712c50c3bf69296ccebff143dc08b58e
Ceuca <adda.ceuca@yahoo.com>
2020-12-12 11:56:05 +0200

    about page

--- js.js
+++ js.js
@@ -7,3 +7,22 @@ function load_contact() {
 function load_portofolio() {
   document.getElementById("mainContent").innerHTML='<object type="text/html" data="../portofoliopage/portofolio.html"></object>';
 }
+
+function load_workExperience(param) {
+  const options= ['school','work1','work2','work3'];
+  options.forEach(option => {
+    document.getElementById(option).style.display= option==param ? "block" : "none";
+    document.getElementById(`button_${option}`).classList = option==param ? "selected" : ""
+  })
+}
+
+function load_education(param) {
+  const options= ['master','university','highschool'];
+  options.forEach(option => {
+    document.getElementById(option).style.display= option==param ? "block" : "none";
+    document.getElementById(`button_${option}`).classList = option==param ? "selected" : ""
+  })
+}
+
+
+
